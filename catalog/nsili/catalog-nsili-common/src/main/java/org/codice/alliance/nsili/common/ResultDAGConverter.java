codice/NSILI

CAL-64: Correctly populate the NSIL_CARD.status field. (#107)
- History data used to populate OBSOLETE records
- Queries updated as well to match NSILI default inclusion of OBSOLETE records.

catalog/nsili/catalog-nsili-common/src/main/java/org/codice/alliance/nsili/common/ResultDAGConverter.java
@@ -13,6 +13,7 @@
  */
 package org.codice.alliance.nsili.common;
 
+import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
@@ -61,9 +62,11 @@ import org.slf4j.LoggerFactory;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.ParseException;
 
+import ddf.catalog.core.versioning.MetacardVersion;
 import ddf.catalog.data.Attribute;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.Result;
+import ddf.catalog.data.types.Core;
 
 public class ResultDAGConverter {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResultDAGConverter.class);
@@ -241,11 +244,31 @@ public class ResultDAGConverter {
         }
 
         if (shouldAdd(buildAttr(attribute, NsiliConstants.STATUS), resultAttributes)) {
-            addStringAttribute(graph,
-                    cardNode,
-                    NsiliConstants.STATUS,
-                    NsiliCardStatus.CHANGED.name(),
-                    orb);
+            String status = NsiliCardStatus.CHANGED.name();
+            Attribute createdAttr = metacard.getAttribute(Core.METACARD_CREATED);
+            Attribute modifiedAttr = metacard.getAttribute(Core.METACARD_MODIFIED);
+            if (createdAttr != null && modifiedAttr != null) {
+                Date createdDate = (Date) createdAttr.getValue();
+                Date modifiedDate = (Date) modifiedAttr.getValue();
+                if (createdDate.equals(modifiedDate)) {
+                    status = NsiliCardStatus.NEW.name();
+                }
+            }
+
+            Attribute versionAction = metacard.getAttribute(MetacardVersion.ACTION);
+
+            if (versionAction != null) {
+                for (Serializable action : versionAction.getValues()) {
+                    if (action.toString()
+                            .trim()
+                            .equals(MetacardVersion.Action.DELETED.getKey())) {
+                        status = NsiliCardStatus.OBSOLETE.name();
+                        break;
+                    }
+                }
+            }
+
+            addStringAttribute(graph, cardNode, NsiliConstants.STATUS, status, orb);
             addedAttributes.add(buildAttr(attribute, NsiliConstants.STATUS));
         }
 
@@ -637,8 +660,9 @@ public class ResultDAGConverter {
         }
 
         if (shouldAdd(buildAttr(attribute, NsiliConstants.IDENTIFIER_UUID), resultAttributes)) {
-            if (metacard.getId() != null) {
-                UUID uuid = getUUIDFromCard(metacard.getId());
+            String metacardId = getMetacardId(metacard);
+            if (metacardId != null) {
+                UUID uuid = getUUIDFromCard(metacardId);
                 addStringAttribute(graph,
                         commonNode,
                         NsiliConstants.IDENTIFIER_UUID,
@@ -932,7 +956,11 @@ public class ResultDAGConverter {
     }
 
     public static List<String> getAttributes(DAG dag) {
-        List<String> attributes = new ArrayList<>();
+        return new ArrayList<>(getAttributeMap(dag).keySet());
+    }
+
+    public static Map<String, String> getAttributeMap(DAG dag) {
+        Map<String, String> attributes = new HashMap<>();
 
         Map<Integer, Node> nodeMap = createNodeMap(dag.nodes);
         DirectedAcyclicGraph<Node, Edge> graph = new DirectedAcyclicGraph<>(Edge.class);
@@ -995,7 +1023,7 @@ public class ResultDAGConverter {
                         currEntry++;
                     }
                     attribute += node.attribute_name;
-                    attributes.add(attribute);
+                    attributes.put(attribute, CorbaUtils.getNodeValue(node.value));
                 } else {
                     int lastIdx = nodeStack.size() - 1;
                     nodeStack.remove(lastIdx);
@@ -1148,5 +1176,17 @@ public class ResultDAGConverter {
         }
 
         return dataIsValid.get();
+    }
+
+    public static String getMetacardId(Metacard metacard) {
+        String id = metacard.getId();
+
+        Attribute origIdAttr = metacard.getAttribute(MetacardVersion.VERSION_OF_ID);
+        if (origIdAttr != null && origIdAttr.getValue().toString() != null) {
+            id = origIdAttr.getValue()
+                    .toString();
+        }
+
+        return id;
     }
 }

catalog/nsili/catalog-nsili-common/src/test/java/org/codice/alliance/nsili/common/TestResultDAGConverter.java
@@ -19,38 +19,56 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import org.codice.alliance.nsili.common.UCO.DAG;
+import org.junit.Before;
 import org.junit.Test;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
 
+import ddf.catalog.core.versioning.MetacardVersion;
+import ddf.catalog.data.impl.AttributeImpl;
 import ddf.catalog.data.impl.MetacardImpl;
 import ddf.catalog.data.impl.ResultImpl;
+import ddf.catalog.data.types.Core;
 
 public class TestResultDAGConverter {
+    private static final String CARD_TITLE = "Test Title";
+
+    private static final String CARD_SOURCE = "Test Source";
+
+    private static final String STATUS_ATTR_NAME =
+            NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_CARD + "."
+                    + NsiliConstants.STATUS;
+
+    private static final Date TEST_CREATE_DATE = new Date(1000);
+
+    private ORB orb;
+
+    private POA rootPOA;
+
+    @Before
+    public void setUp() throws AdapterInactive, InvalidName {
+        orb = ORB.init(new String[0], null);
+        rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+        rootPOA.the_POAManager()
+                .activate();
+    }
 
     @Test
     public void testResultAttributes() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
 
         String sourceAttr = NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_CARD + "."
                 + NsiliConstants.SOURCE_LIBRARY;
@@ -75,21 +93,11 @@ public class TestResultDAGConverter {
 
     @Test
     public void testAdvancedGeospatial() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
         card.setLocation("POLYGON((1 1,1 2,2 2,2 1,1 1))");
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
 
         String advGeoAttr = NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_PART + ":"
                 + NsiliConstants.NSIL_COVERAGE + "." + NsiliConstants.ADVANCED_GEOSPATIAL;
@@ -108,23 +116,10 @@ public class TestResultDAGConverter {
 
     @Test(expected = DagParsingException.class)
     public void testMandatoryAttributesFail() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
-
-        String sourceAttr = NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_CARD + "."
-                + NsiliConstants.SOURCE_LIBRARY;
 
         Map<String, List<String>> mandatoryAttrs = new HashMap<>();
         mandatoryAttrs.put(NsiliConstants.NSIL_COMMON,
@@ -138,20 +133,10 @@ public class TestResultDAGConverter {
 
     @Test
     public void testMandatoryAttributesSuccess() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
 
         Map<String, List<String>> mandatoryAttrs = new HashMap<>();
         mandatoryAttrs.put(NsiliConstants.NSIL_CARD, Arrays.asList(NsiliConstants.IDENTIFIER));
@@ -163,8 +148,87 @@ public class TestResultDAGConverter {
         assertThat(dag, notNullValue());
     }
 
+    @Test
+    public void testCreateAttribute() throws Exception {
+        MetacardImpl card = getTestCard();
+
+        ResultImpl result = new ResultImpl();
+        result.setMetacard(card);
+
+        DAG dag = ResultDAGConverter.convertResult(result,
+                orb,
+                rootPOA,
+                new ArrayList<>(),
+                new HashMap<>());
+        assertThat(dag, notNullValue());
+
+        String value = ResultDAGConverter.getAttributeMap(dag)
+                .get(STATUS_ATTR_NAME);
+        assertThat(value, is(NsiliCardStatus.NEW.name()));
+    }
+
+    @Test
+    public void testChangeAttribute() throws Exception {
+        MetacardImpl card = getTestCard();
+        Date testModifiedDate = new Date(2000);
+        card.setAttribute(new AttributeImpl(Core.METACARD_MODIFIED, testModifiedDate));
+
+        ResultImpl result = new ResultImpl();
+        result.setMetacard(card);
+
+        DAG dag = ResultDAGConverter.convertResult(result,
+                orb,
+                rootPOA,
+                new ArrayList<>(),
+                new HashMap<>());
+        assertThat(dag, notNullValue());
+
+        String value = ResultDAGConverter.getAttributeMap(dag)
+                .get(STATUS_ATTR_NAME);
+        assertThat(value, is(NsiliCardStatus.CHANGED.name()));
+    }
+
+    @Test
+    public void testDeleteAttribute() throws Exception {
+        MetacardImpl card = getTestCard();
+        Date testModifiedDate = new Date(2000);
+        card.setModifiedDate(testModifiedDate);
+        card.setAttribute(new AttributeImpl(MetacardVersion.ACTION,
+                MetacardVersion.Action.DELETED.getKey()));
+
+        ResultImpl result = new ResultImpl();
+        result.setMetacard(card);
+
+        DAG dag = ResultDAGConverter.convertResult(result,
+                orb,
+                rootPOA,
+                new ArrayList<>(),
+                new HashMap<>());
+        assertThat(dag, notNullValue());
+
+        String value = ResultDAGConverter.getAttributeMap(dag)
+                .get(STATUS_ATTR_NAME);
+        assertThat(value, is(NsiliCardStatus.OBSOLETE.name()));
+    }
+
     private static boolean checkDagContains(DAG dag, String attribute) {
         List<String> dagAttrs = ResultDAGConverter.getAttributes(dag);
         return dagAttrs.contains(attribute);
+    }
+
+    private MetacardImpl getTestCard() {
+        String id = UUID.randomUUID()
+                .toString();
+
+        MetacardImpl card = new MetacardImpl();
+        card.setId(id);
+        card.setTitle(CARD_TITLE);
+        card.setSourceId(CARD_SOURCE);
+        card.setAttribute(new AttributeImpl(Core.METACARD_CREATED, TEST_CREATE_DATE));
+        card.setCreatedDate(TEST_CREATE_DATE);
+        card.setAttribute(new AttributeImpl(Core.METACARD_MODIFIED, TEST_CREATE_DATE));
+        card.setModifiedDate(TEST_CREATE_DATE);
+
+        return card;
     }
 }

vinamartin/alliance

Conflicts:
	catalog/nsili/catalog-nsili-endpoint/pom.xml
	catalog/nsili/catalog-nsili-transformer/src/main/java/org/codice/alliance/nsili/transformer/AnyConverter.java
	catalog/nsili/catalog-nsili-transformer/src/main/java/org/codice/alliance/nsili/transformer/DAGConverter.java

catalog/nsili/catalog-nsili-common/src/main/java/org/codice/alliance/nsili/common/ResultDAGConverter.java
@@ -13,6 +13,7 @@
  */
 package org.codice.alliance.nsili.common;
 
+import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
@@ -61,6 +62,7 @@ import org.slf4j.LoggerFactory;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.ParseException;
 
+import ddf.catalog.core.versioning.MetacardVersion;
 import ddf.catalog.data.Attribute;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.Result;
@@ -241,11 +243,31 @@ public class ResultDAGConverter {
         }
 
         if (shouldAdd(buildAttr(attribute, NsiliConstants.STATUS), resultAttributes)) {
-            addStringAttribute(graph,
-                    cardNode,
-                    NsiliConstants.STATUS,
-                    NsiliCardStatus.CHANGED.name(),
-                    orb);
+            String status = NsiliCardStatus.CHANGED.name();
+            Attribute createdAttr = metacard.getAttribute(Metacard.CREATED);
+            Attribute modifiedAttr = metacard.getAttribute(Metacard.MODIFIED);
+            if (createdAttr != null && modifiedAttr != null) {
+                Date createdDate = (Date) createdAttr.getValue();
+                Date modifiedDate = (Date) modifiedAttr.getValue();
+                if (createdDate.equals(modifiedDate)) {
+                    status = NsiliCardStatus.NEW.name();
+                }
+            }
+
+            Attribute versionAction = metacard.getAttribute(MetacardVersion.ACTION);
+
+            if (versionAction != null) {
+                for (Serializable action : versionAction.getValues()) {
+                    if (action.toString()
+                            .trim()
+                            .equals(MetacardVersion.Action.DELETED.getKey())) {
+                        status = NsiliCardStatus.OBSOLETE.name();
+                        break;
+                    }
+                }
+            }
+
+            addStringAttribute(graph, cardNode, NsiliConstants.STATUS, status, orb);
             addedAttributes.add(buildAttr(attribute, NsiliConstants.STATUS));
         }
 
@@ -637,8 +659,9 @@ public class ResultDAGConverter {
         }
 
         if (shouldAdd(buildAttr(attribute, NsiliConstants.IDENTIFIER_UUID), resultAttributes)) {
-            if (metacard.getId() != null) {
-                UUID uuid = getUUIDFromCard(metacard.getId());
+            String metacardId = getMetacardId(metacard);
+            if (metacardId != null) {
+                UUID uuid = getUUIDFromCard(metacardId);
                 addStringAttribute(graph,
                         commonNode,
                         NsiliConstants.IDENTIFIER_UUID,
@@ -932,7 +955,11 @@ public class ResultDAGConverter {
     }
 
     public static List<String> getAttributes(DAG dag) {
-        List<String> attributes = new ArrayList<>();
+        return new ArrayList<>(getAttributeMap(dag).keySet());
+    }
+
+    public static Map<String, String> getAttributeMap(DAG dag) {
+        Map<String, String> attributes = new HashMap<>();
 
         Map<Integer, Node> nodeMap = createNodeMap(dag.nodes);
         DirectedAcyclicGraph<Node, Edge> graph = new DirectedAcyclicGraph<>(Edge.class);
@@ -995,7 +1022,7 @@ public class ResultDAGConverter {
                         currEntry++;
                     }
                     attribute += node.attribute_name;
-                    attributes.add(attribute);
+                    attributes.put(attribute, CorbaUtils.getNodeValue(node.value));
                 } else {
                     int lastIdx = nodeStack.size() - 1;
                     nodeStack.remove(lastIdx);
@@ -1148,5 +1175,17 @@ public class ResultDAGConverter {
         }
 
         return dataIsValid.get();
+    }
+
+    public static String getMetacardId(Metacard metacard) {
+        String id = metacard.getId();
+
+        Attribute origIdAttr = metacard.getAttribute(MetacardVersion.VERSION_OF_ID);
+        if (origIdAttr != null && origIdAttr.getValue().toString() != null) {
+            id = origIdAttr.getValue()
+                    .toString();
+        }
+
+        return id;
     }
 }

catalog/nsili/catalog-nsili-common/src/test/java/org/codice/alliance/nsili/common/TestResultDAGConverter.java
@@ -19,38 +19,56 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import org.codice.alliance.nsili.common.UCO.DAG;
+import org.junit.Before;
 import org.junit.Test;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
 
+import ddf.catalog.core.versioning.MetacardVersion;
+import ddf.catalog.data.Metacard;
+import ddf.catalog.data.impl.AttributeImpl;
 import ddf.catalog.data.impl.MetacardImpl;
 import ddf.catalog.data.impl.ResultImpl;
 
 public class TestResultDAGConverter {
+    private static final String CARD_TITLE = "Test Title";
+
+    private static final String CARD_SOURCE = "Test Source";
+
+    private static final String STATUS_ATTR_NAME =
+            NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_CARD + "."
+                    + NsiliConstants.STATUS;
+
+    private static final Date TEST_CREATE_DATE = new Date(1000);
+
+    private ORB orb;
+
+    private POA rootPOA;
+
+    @Before
+    public void setUp() throws AdapterInactive, InvalidName {
+        orb = ORB.init(new String[0], null);
+        rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+        rootPOA.the_POAManager()
+                .activate();
+    }
 
     @Test
     public void testResultAttributes() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
 
         String sourceAttr = NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_CARD + "."
                 + NsiliConstants.SOURCE_LIBRARY;
@@ -75,21 +93,11 @@ public class TestResultDAGConverter {
 
     @Test
     public void testAdvancedGeospatial() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
         card.setLocation("POLYGON((1 1,1 2,2 2,2 1,1 1))");
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
 
         String advGeoAttr = NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_PART + ":"
                 + NsiliConstants.NSIL_COVERAGE + "." + NsiliConstants.ADVANCED_GEOSPATIAL;
@@ -108,23 +116,10 @@ public class TestResultDAGConverter {
 
     @Test(expected = DagParsingException.class)
     public void testMandatoryAttributesFail() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
-
-        String sourceAttr = NsiliConstants.NSIL_PRODUCT + ":" + NsiliConstants.NSIL_CARD + "."
-                + NsiliConstants.SOURCE_LIBRARY;
 
         Map<String, List<String>> mandatoryAttrs = new HashMap<>();
         mandatoryAttrs.put(NsiliConstants.NSIL_COMMON,
@@ -138,20 +133,10 @@ public class TestResultDAGConverter {
 
     @Test
     public void testMandatoryAttributesSuccess() throws Exception {
-        String id = UUID.randomUUID()
-                .toString();
-        MetacardImpl card = new MetacardImpl();
-        card.setId(id);
-        card.setTitle("Test Title");
-        card.setSourceId("Test Source");
+        MetacardImpl card = getTestCard();
 
         ResultImpl result = new ResultImpl();
         result.setMetacard(card);
-
-        ORB orb = ORB.init(new String[0], null);
-        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        rootPOA.the_POAManager()
-                .activate();
 
         Map<String, List<String>> mandatoryAttrs = new HashMap<>();
         mandatoryAttrs.put(NsiliConstants.NSIL_CARD, Arrays.asList(NsiliConstants.IDENTIFIER));
@@ -163,8 +148,87 @@ public class TestResultDAGConverter {
         assertThat(dag, notNullValue());
     }
 
+    @Test
+    public void testCreateAttribute() throws Exception {
+        MetacardImpl card = getTestCard();
+
+        ResultImpl result = new ResultImpl();
+        result.setMetacard(card);
+
+        DAG dag = ResultDAGConverter.convertResult(result,
+                orb,
+                rootPOA,
+                new ArrayList<>(),
+                new HashMap<>());
+        assertThat(dag, notNullValue());
+
+        String value = ResultDAGConverter.getAttributeMap(dag)
+                .get(STATUS_ATTR_NAME);
+        assertThat(value, is(NsiliCardStatus.NEW.name()));
+    }
+
+    @Test
+    public void testChangeAttribute() throws Exception {
+        MetacardImpl card = getTestCard();
+        Date testModifiedDate = new Date(2000);
+        card.setAttribute(new AttributeImpl(Metacard.MODIFIED, testModifiedDate));
+
+        ResultImpl result = new ResultImpl();
+        result.setMetacard(card);
+
+        DAG dag = ResultDAGConverter.convertResult(result,
+                orb,
+                rootPOA,
+                new ArrayList<>(),
+                new HashMap<>());
+        assertThat(dag, notNullValue());
+
+        String value = ResultDAGConverter.getAttributeMap(dag)
+                .get(STATUS_ATTR_NAME);
+        assertThat(value, is(NsiliCardStatus.CHANGED.name()));
+    }
+
+    @Test
+    public void testDeleteAttribute() throws Exception {
+        MetacardImpl card = getTestCard();
+        Date testModifiedDate = new Date(2000);
+        card.setModifiedDate(testModifiedDate);
+        card.setAttribute(new AttributeImpl(MetacardVersion.ACTION,
+                MetacardVersion.Action.DELETED.getKey()));
+
+        ResultImpl result = new ResultImpl();
+        result.setMetacard(card);
+
+        DAG dag = ResultDAGConverter.convertResult(result,
+                orb,
+                rootPOA,
+                new ArrayList<>(),
+                new HashMap<>());
+        assertThat(dag, notNullValue());
+
+        String value = ResultDAGConverter.getAttributeMap(dag)
+                .get(STATUS_ATTR_NAME);
+        assertThat(value, is(NsiliCardStatus.OBSOLETE.name()));
+    }
+
     private static boolean checkDagContains(DAG dag, String attribute) {
         List<String> dagAttrs = ResultDAGConverter.getAttributes(dag);
         return dagAttrs.contains(attribute);
+    }
+
+    private MetacardImpl getTestCard() {
+        String id = UUID.randomUUID()
+                .toString();
+
+        MetacardImpl card = new MetacardImpl();
+        card.setId(id);
+        card.setTitle(CARD_TITLE);
+        card.setSourceId(CARD_SOURCE);
+        card.setAttribute(new AttributeImpl(Metacard.CREATED, TEST_CREATE_DATE));
+        card.setCreatedDate(TEST_CREATE_DATE);
+        card.setAttribute(new AttributeImpl(Metacard.MODIFIED, TEST_CREATE_DATE));
+        card.setModifiedDate(TEST_CREATE_DATE);
+
+        return card;
     }
 }