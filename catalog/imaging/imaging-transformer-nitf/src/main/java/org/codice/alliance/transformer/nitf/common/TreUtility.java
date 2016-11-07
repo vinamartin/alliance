883ba5c4b2ef6d06baffb1386a7e89b8e2278ce0
troymohl <troy.mohl@connexta.com>
2016-11-07 12:06:15 -0700

    CAL-134: Common TreUtility for processing CSEXRA, CSDIDA, PIAIMC, and HISTOA TREs (#179) (#196)

--- catalog/imaging/imaging-transformer-nitf/src/main/java/org/codice/alliance/transformer/nitf/common/TreUtility.java
+++ catalog/imaging/imaging-transformer-nitf/src/main/java/org/codice/alliance/transformer/nitf/common/TreUtility.java
@@ -13,20 +13,32 @@
  */
 package org.codice.alliance.transformer.nitf.common;
 
-import java.io.Serializable;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Optional;
+import java.util.TimeZone;
 
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.codice.imaging.nitf.core.common.NitfFormatException;
+import org.codice.imaging.nitf.core.tre.Tre;
 import org.codice.imaging.nitf.core.tre.TreGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class TreUtility {
+    static final String TRE_DATE_FORMAT = "yyyyMMddkkmmss";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(TreUtility.class);
+
+    private static final FastDateFormat DATE_FORMATTER = FastDateFormat.getInstance(TRE_DATE_FORMAT, TimeZone.getTimeZone("GMT"));
 
     private TreUtility() {
     }
 
-    public static Serializable getTreValue(TreGroup tre, String key) {
+    @Nullable
+    public static String getTreValue(TreGroup tre, String key) {
         try {
             String value = tre.getFieldValue(key);
 
@@ -36,7 +48,68 @@ public final class TreUtility {
 
             return value;
         } catch (NitfFormatException e) {
-            LOGGER.warn(e.getMessage(), e);
+            LOGGER.debug(e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public static String convertToString(Tre tre, String fieldName) {
+        return TreUtility.getTreValue(tre, fieldName);
+    }
+
+    @Nullable
+    public static Integer convertToInteger(Tre tre, String fieldName) {
+        String value = TreUtility.getTreValue(tre, fieldName);
+        if (value != null) {
+            return Integer.valueOf(value);
+        } else {
+            return null;
+        }
+    }
+
+    public static Optional<Integer> findIntValue(Tre tre, String tagName) {
+        try {
+            return Optional.of(tre.getIntValue(tagName));
+        } catch (NitfFormatException e) {
+            LOGGER.debug("failed to find {}", tagName, e);
+        }
+        return Optional.empty();
+    }
+
+    @Nullable
+    public static Float convertToFloat(Tre tre, String fieldName) {
+        String value = TreUtility.getTreValue(tre, fieldName);
+        if (value != null) {
+            return Float.valueOf(value);
+        } else {
+            return null;
+        }
+    }
+
+    @Nullable
+    public static Boolean convertYnToBoolean(Tre tre, String fieldName) {
+        String value = TreUtility.getTreValue(tre, fieldName);
+        if (value != null) {
+            return value.equalsIgnoreCase("Y");
+        } else {
+            return null;
+        }
+    }
+
+    @Nullable
+    public static Date convertToDate(Tre tre, String fieldName) {
+        String value = TreUtility.getTreValue(tre, fieldName);
+        if (value != null) {
+            try {
+                return DATE_FORMATTER.parse(value);
+            } catch (ParseException e) {
+                LOGGER.debug("Unable to parse date {} according to format {}",
+                        value,
+                        TRE_DATE_FORMAT,
+                        e);
+            }
         }
 
         return null;
